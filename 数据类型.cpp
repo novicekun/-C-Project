18115772aec7bad8c5bda09771427a71f15e4226
novicekun <136944131+novicekun@users.noreply.github.com>
2023-06-18 11:38:15 +0800

    Add files via upload

---
+++ 数据类型.cpp
@@ -0,0 +1,15 @@
+ #define  _CRT_SECURE_NO_WARNINGS
+ #include<stdio.h>
+//数据类型
+int main()
+{
+	int a;//整形
+	short b;//短整形(也可以写成 short int）
+	long c;//长整形（同上 long int)
+	long long d;//更长的整形
+	char e;//字符数据类型
+	//char e = 'a';
+	float f;//单精度浮点数
+	double g;//双精度浮点数
+	return 0;
+}
