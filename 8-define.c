aebeb64898013c51d21bd993f45342e23be97d54
novicekun <136944131+novicekun@users.noreply.github.com>
2023-06-23 20:50:59 +0800

    Add files via upload

---
+++ 8-define.c
@@ -0,0 +1,12 @@
+ #define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#define NUM 1000//define定义常量
+#define ADD(x,y) x+y
+int main()
+{
+	printf("%d\n", NUM);
+	printf("%d\n", ADD(1,2));//结果为3
+	printf("%d\n", 2*ADD(1,2));//结果为4，因为定义的宏是替换，表达式可以表示成2*1+2
+
+	return 0;
+} 
