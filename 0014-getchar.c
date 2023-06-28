9bf193a6ec991d9ec6e940b4adb3d11e2e929f04
novicekun <136944131+novicekun@users.noreply.github.com>
2023-06-28 11:49:28 +0800

    Add files via upload

---
+++ 0014-getchar.c
@@ -0,0 +1,12 @@
+ #define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//getchar在键盘上获取一个字符
+//EOF---end of file文件结束标志，ctrl+z结束读取
+//putchar输出一个字符
+
+int main()
+{
+	int ch = getchar();
+	putchar(ch);
+
+}
